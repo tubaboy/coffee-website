bd16e1a509d4883d52b2d1987cb52030afe0399a
David <tawei.chuang@gmail.com>
2025-12-12 10:21:05 +0800

    Fix TS6133 error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, (process as any).cwd(), '')
-
-  return {
-    plugins: [react()],
-    // 修改為 '/' 以適配 Zeabur 的根目錄部署
-    base: '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  // 修改為 '/' 以適配 Zeabur 的根目錄部署
+  base: '/',
 })
